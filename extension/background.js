1b6c1cc0792379f5463a73976e062781261aa8de
承风 <libiao.lb@taobao.com>
2014-11-09 16:15:48 +0800

    fix bug of link click in panel & no response for page nav

--- extension/background.js
+++ extension/background.js
@@ -2,29 +2,52 @@
 var ports = {};
 
 // Function to send a message to all devtools.html views:
-function notifyDevtools(msg, id, url) {
+function notifyDevtools(msg, id) {
     var port = ports[id];
-    if(port.port && port.url === url) {
-        port.port.postMessage(msg);
+    if(port) {
+        port.postMessage(msg);
     }
 }
 
+//reset data
+chrome.runtime.onStartup.addListener(function() {
+    ports = {};
+});
+
 chrome.runtime.onConnect.addListener(function(port) {
+    //fixed error when other extensions fire connect event
+    if(!port) {
+        return;
+    }
     var names = port.name.split('_');
-    if (!names || names.length !== 3 || names[0] !== "performance") {
+    if (!names || names.length !== 2 || names[0] !== "performance") {
         return;
     }
     curTabId = names[1];
-    ports[curTabId] =  {
-        port : port,
-        url : names[2]
-    };
+    ports[curTabId] =  port;
     // Remove port when destroyed (eg when devtools instance is closed)
     port.onDisconnect.addListener(function () {
         delete ports[curTabId];
     });
 });
 
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    notifyDevtools('reloadcomplete', details.tabId, details.url);
+//Trigger with frames and subFrames such as iframe!
+//chrome.webNavigation.onCompleted.addListener(function(details) {
+//    notifyDevtools('reloadcomplete', details.tabId, details.url);
+//});
+
+//Only trigger if user use History API
+//chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+//    console.log('Page uses History API and we heard a pushSate/replaceState.');
+//    // do your thing
+//});
+
+//Only trigger if hash change
+//chrome.webNavigation.onReferenceFragmentUpdated.addListener(function(detail) {
+//    debugger
+//});
+chrome.tabs.onUpdated.addListener(function(id, info, tab) {
+    if(info.status === 'complete') {
+        notifyDevtools('reloadcomplete', id);
+    }
 });
